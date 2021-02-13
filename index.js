aa98eb060d462a5cbee1282ab6917cb714f90dbf
Paquitos <78994288+paquitosk6@users.noreply.github.com>
2021-02-13 10:19:57 -0300

    update chat msg

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 var btnEntrar = document.getElementById("btnEntrar");
+var chatMsg = document.getElementById("chatBox");
+var btnChat = document.getElementById("chat");
 
 btnEntrar.addEventListener("click", () => {
     alert("Aeeeee, ti fofo!");
+})
+
+btnChat.addEventListener("click", () => {
+
+    if (chatMsg.style.display == "none") {
+        chatMsg.style.display = "flex";
+    } else {
+        chatMsg.style.display = "none";
+    }
+
 })
